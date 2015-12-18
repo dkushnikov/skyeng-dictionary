61b8b6e617109960c27cba955a359b8f97435279
Dmitry Kushnikov <dkushnikov@gmail.com>
2015-12-18 03:21:28 +0300

    Add dictionary to parameters

--- app/config/params.php
+++ app/config/params.php
@@ -1,5 +1,30 @@
 <?php
 
+use yii\helpers\Json;
+
 return [
     'adminEmail' => 'admin@example.com',
+    'dictionary' => Json::decode('
+        {
+            "apple": "яблоко",
+            "pear": "персик",
+            "orange": "апельсин",
+            "grape": "виноград",
+            "lemon": "лимон",
+            "pineapple": "ананас",
+            "watermelon": "арбуз",
+            "coconut": "кокос",
+            "banana": "банан",
+            "pomelo": "помело",
+            "strawberry": "клубника",
+            "raspberry": "малина",
+            "melon": "дыня",
+            "apricot": "абрикос",
+            "mango": "манго",
+            "pear": "слива",
+            "pomegranate": "гранат",
+            "cherry": "вишня"
+        }
+    '
+    ),
 ];
